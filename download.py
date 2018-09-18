c6ae35ca5545fa9c7b4a721f419d57fb7a08f0c8
Vladimir Grichina <vgrichina@componentix.com>
2018-09-18 00:53:00 -0700

    Fix to support slight changes on website

--- download.py
+++ download.py
@@ -14,9 +14,12 @@ all_projects = {}
 for l in links[::-1]:
     print("Downloading: " + l)
     pdf_file = "data/" + re.search("BlobID=([\d]+)", l).group(1) + ".pdf"
-    os.system("curl " + l + " -o " + pdf_file)
+    os.system("curl -L " + l + " -o " + pdf_file)
     os.system("pdftotext -layout " + pdf_file)
     txt_file = pdf_file.replace(".pdf", ".txt")
+    # Clean bad UTF-8 characters
+    os.system("iconv -f utf-8 -t utf-8 -c < " + txt_file + " > tmp.txt")
+    os.system("mv tmp.txt " + txt_file)
     parsed = parse.parse(txt_file, l)
     for p in parsed:
         all_projects[p["title"]] = p
